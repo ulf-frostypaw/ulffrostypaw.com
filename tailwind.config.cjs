24a528712931c8742890584eb2d0e49f1929d509
Ulf FrostyPaw <ulffrostypaw@gmail.com>
2025-04-26 09:17:07 -0600

    simplificar la interfaz princial y quitar blogs, tags y otros contenidos

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}', './index.html'],
   darkMode: 'class',
   theme: {
     extend: {
